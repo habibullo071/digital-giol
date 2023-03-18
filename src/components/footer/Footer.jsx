d2b2782f7684064c6e80ddb8b32f5c6c0cce8a58
habibullo071 <habibulloqodirov2006@gmail.com>
2023-03-18 12:58:01 +0300

    hhagvh

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,40 +1,65 @@
 import React from 'react'
 import './Footer.css'
 import img1 from '../../img/img1.png'
+import img12 from '../../img/img12.png'
 function Footer() {
-  return (
-    <div className='Footer'>
-        <div className="foot">
-            
-        <ul>
-            <li className='footer__left'>
-                <img src={img1} alt="" />
-                <h2>GIRLS IN DIGITAL</h2>
-            </li>
-            <li className='footer__centr'>
+    return (
+        <div className='Footer'>
+            <div className="foot">
+
                 <ul>
-                    <li>
-                        <p><a href="#proekt">О Проекте </a></p>
+                    <li className='footer__left'>
+                        <a href="#"><img src={img1} alt="" /></a>
+                        <h2>GIRLS IN DIGITAL</h2>
                     </li>
-                    <li>
-                        <p><a href="#naprav">Направления</a></p>
+                    <li className='footer__centr'>
+                        <ul>
+                            <li>
+                                <p><a href="#proekt">О Проекте </a></p>
+                            </li>
+                            <li>
+                                <p><a href="#naprav">Направления</a></p>
+                            </li>
+                            <li>
+                                <p><a href="#result">Результаты</a></p>
+                            </li>
+                            <li>
+                                <p><a href="#map">Контакты</a></p>
+                            </li>
+                        </ul>
                     </li>
-                    <li>
-                        <p><a href="#result">Результаты</a></p>
-                    </li>
-                    <li>
-                        <p><a href="#map">Контакты</a></p>
+                    <li className='footer__right'>
+                        <ul className='ol'>
+                            <button>Uz</button>
+                            <button>Ru</button>
+                        </ul>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src={img12} alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={img12} alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={img12} alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={img12} alt="" />
+                                </a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
-            </li>
-            <li className='footer__right'>
-                <b>Uz</b>
-                <b>Uz</b>
-            </li>
-        </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
